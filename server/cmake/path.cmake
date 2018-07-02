6af6504bfd1eee7078cd69838788b98ed00a31e6
Praying <qurandeyouxiang@qq.com>
2018-07-03 06:35:09 +0800

    modify cmake setting

--- server/cmake/path.cmake
+++ server/cmake/path.cmake
@@ -9,5 +9,12 @@ else()
     set(LibOutPutDir ${PROJECT_SOURCE_DIR}/lib/release)
 endif()
 
-#set(EXECUTABLE_OUTPUT_PATH ${BinOutPutDir})
-#set(LIBRARY_OUTPUT_PATH ${LibOutPutDir})
+#Executables are always treated as runtime targets
+set(EXECUTABLE_OUTPUT_PATH  ${BinOutPutDir})
+set(RUNTIME_OUTPUT_DIRECTORY ${BinOutPutDir})
+#Static libraries are always treated as archive targets
+set(ARCHIVE_OUTPUT_DIRECTORY ${LibOutPutDir})
+#Module libraries are always treated as library targets.
+#For non-DLL platforms shared libraries are treated as library targets.
+#For DLL platforms the DLL part of a shared library is treated as a runtime target and the corresponding import library is treated as an archive target
+set(LIBRARY_OUTPUT_PATH ${LibOutPutDir})
